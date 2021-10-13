1e742ac56ce647b009bc2ffa180e98d4114480ae
Alexander Nookinov <anookinov@gmail.com>
2021-10-13 16:42:18 +0700

    create StrSplit struct and test it

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//!
+// #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+
+#[derive(Debug)]
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
+}
+
+impl<'a> StrSplit<'a> {
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
     }
+}
+
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if self.remainder.is_empty() {
+            // TODO: bug
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = "";
+            Some(rest)
+        }
+    }
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack, " ");
+    assert!(letters.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
 }
