4048ff2cb3928373cc49cfbfe76da967fd9df96f
Alexander Nookinov <anookinov@gmail.com>
2021-10-13 18:10:48 +0700

    fix the bug related to the case that delimiter is at the end

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,14 @@
 
 #[derive(Debug)]
 pub struct StrSplit<'a> {
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str,
 }
 
 impl<'a> StrSplit<'a> {
     pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter,
         }
     }
@@ -19,17 +19,16 @@ impl<'a> StrSplit<'a> {
 impl<'a> Iterator for StrSplit<'a> {
     type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next_delim) = self.remainder.find(self.delimiter) {
-            let until_delimiter = &self.remainder[..next_delim];
-            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
-            Some(until_delimiter)
-        } else if self.remainder.is_empty() {
-            // TODO: bug
-            None
+        if let Some(ref mut remainder) = self.remainder {
+            if let Some(next_delim) = remainder.find(self.delimiter) {
+                let until_delimiter = &remainder[..next_delim];
+                *remainder = &remainder[(next_delim + self.delimiter.len())..];
+                Some(until_delimiter)
+            } else {
+                self.remainder.take()
+            }
         } else {
-            let rest = self.remainder;
-            self.remainder = "";
-            Some(rest)
+            None
         }
     }
 }
@@ -39,4 +38,11 @@ fn it_works() {
     let haystack = "a b c d e";
     let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
     assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
+}
+
+#[test]
+fn tail() {
+    let haystack = "a b c d ";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
 }
